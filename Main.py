c6db331c83c88f0372cd79499f015383f7c0ef60
wisscot <wisscot.register@gmail.com>
2020-07-06 15:06:15 -0400

    refactoring

--- Main.py
+++ Main.py
@@ -11,8 +11,9 @@ import pprint
 import datetime
 import random
 
-LOGINURL = r"https://www.amazon.com/ap/signin?_encoding=UTF8&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26action%3Dsign-out%26path%3D%252Fgp%252Fyourstore%252Fhome%26ref_%3Dnav_AccountFlyout_signout%26signIn%3D1%26useRedirectOnSuccess%3D1"
+LOGINURL = r"https://www.amazon.com/"
 PRODUCT_URL = r"https://www.amazon.com/Ring-Fit-Adventure-Nintendo-Switch/dp/B07XV4NHHN/ref=sr_1_1?dchild=1&keywords=ring+fit&qid=1593802327&sr=8-1"
+NOTIFY_URL = os.environ.get('ftqqURL')
 
 class ChromeBrowser:
 
@@ -67,7 +68,7 @@ class ChromeBrowser:
                 return
             
     def notify(self, text):
-        url = os.environ.get('ftqqURL')
+        url = NOTIFY_URL
         url = url + f"?text={text} seed{random.randint(1,100)}"
         r = requests.get(url)
         print(r)
